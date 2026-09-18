abc7e55cf23b5c61a5aaee6e69d38a0139644184
Ansh kr singh <anshkrsingh321@gmail.com>
2026-09-18 13:27:58 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import PolicySource from "./components/PolicySource";
 import AuditTrail from "./components/AuditTrail";
 import "./index.css";
 
-//  backend url
+
 const API_URL =
   import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
